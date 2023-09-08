09bf89dc80c87a660163d4818bf3f1dad64498d3
Jenny Sun <xmissj96@gmail.com>
2023-09-08 12:30:09 +0200

    feat: initial commit.

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<script lang="ts">
+  let count = 0;
+</script>
+
+<h1>Counter: {count}</h1>
+<button on:click={() => count++}>Increase</button>
+<button on:click={() => count--}>Decrease</button>
+<button on:click={() => count *= 2}>Double</button>
