2156e1b0280fbc1b46f726b8f30dc9413abff86b
Jenny Sun <xmissj96@gmail.com>
2023-09-13 13:25:58 +0200

    feat: change file names.

--- src/components/xMenu.svelte
+++ src/components/xMenu.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
-  import Home from "../pages/home.svelte";
-  import About from "../pages/about.svelte";
-  import Projects from "../pages/projects.svelte";
-  import Contact from "../pages/contact.svelte";
+  import Home from "../pages/Home.svelte";
+  import About from "../pages/About.svelte";
+  import Projects from "../pages/Projects.svelte";
+  import Contact from "../pages/Contact.svelte";
 
   let activeMenuItem = 1;
 
@@ -38,23 +38,25 @@
   }
 </script>
 
-<ul id="menu">
-  {#each menuItems as menuItem, index}
-    <li>
-      <!-- svelte-ignore a11y-no-static-element-interactions -->
-      <!-- svelte-ignore a11y-click-events-have-key-events -->
-      <a
-        id="menu-link"
-        on:click|preventDefault={() => navigateTo(menuItem.index)}
-      >
-        {menuItem.label}
-      </a>
-    </li>
-    {#if index !== menuItems.length - 1}
-      <span />
-    {/if}
-  {/each}
-</ul>
+<div id="menu-container">
+  <ul id="menu">
+    {#each menuItems as menuItem, index}
+      <li>
+        <!-- svelte-ignore a11y-no-static-element-interactions -->
+        <!-- svelte-ignore a11y-click-events-have-key-events -->
+        <a
+          id="menu-link"
+          on:click|preventDefault={() => navigateTo(menuItem.index)}
+        >
+          {menuItem.label}
+        </a>
+      </li>
+      {#if index !== menuItems.length - 1}
+        <span />
+      {/if}
+    {/each}
+  </ul>
+</div>
 
 {#if activeMenuItem === 1 || activeMenuItem === 2}
   {#each menuItems as menuItem}
@@ -67,6 +69,16 @@
 {/if}
 
 <style lang="scss">
+  #menu-container {
+    width: 100vw;
+    height: 100px;
+  }
+
+  #menu {
+    padding: 0;
+    text-align: center;
+  }
+
   #menu-link {
     text-decoration: none;
     color: pink;
@@ -77,13 +89,9 @@
     }
   }
 
-  #menu {
-    padding: 0;
-  }
-
   ul#menu li {
     display: inline;
     margin-right: 10px;
-    font-family: "Roboto Light", sans-serif;
+    font-family: "Inter", sans-serif;
   }
 </style>
