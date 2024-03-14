dc15d81d0c89d1b1c8157d2f5a83d5dbe42c37c0
Jenny Sun <xmissj96@gmail.com>
2024-03-14 13:44:00 +0100

    feat: updated packages and also customised menu item.

--- src/components/MenuItem.svelte
+++ src/components/MenuItem.svelte
@@ -1,24 +1,33 @@
 <script lang="ts">
   export let label: string;
-  export let callback: () => void;
+  export let onClick: () => void;
 </script>
 
 <li>
   <!-- svelte-ignore a11y-no-static-element-interactions -->
   <!-- svelte-ignore a11y-click-events-have-key-events -->
-  <a id="menu-link" on:click|preventDefault={callback}>
+  <a id="menu-link" on:click|preventDefault={onClick}>
     {label}
   </a>
 </li>
 
 <style lang="scss">
   #menu-link {
-    text-decoration: none;
-    color: pink;
+    text-decoration: underline;
+    text-underline-offset: 6px; 
+    text-decoration-thickness: 1px;
+    text-decoration-color: rgb(230, 215, 255, 0);
+    color: #e6d7ff;
     transition: color 0.3s;
 
     &:hover {
-      color: white;
+      text-decoration-color: rgb(230, 215, 255, 1);
+    }
+
+    &:active{
+      color: #d2afff;
+      text-decoration-color: #d2afff;
+      transition: color 0.3s;
     }
   }
 
