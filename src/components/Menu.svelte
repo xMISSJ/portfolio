88fb559e677603d9ddd8522269e13ffeadf507fc
Jenny Sun <xmissj96@gmail.com>
2024-03-15 13:36:02 +0100

    feat: improved check and removed test text.

--- src/components/Menu.svelte
+++ src/components/Menu.svelte
@@ -19,15 +19,13 @@
     {#each Object.entries(paths) as [key, path]}
       <a
         href="{base}{path}"
-        class:selected={$page.url.pathname ==
-          (dev ? path : path == "/" ? base + path : base + path + "/")}
+        class:selected={$page.url.pathname ===
+          (dev ? path : (path === "/" ? base : base + path) + "/")}
         >{key.charAt(0).toUpperCase() + key.slice(1)}</a
       >
     {/each}
   </nav>
 </div>
-
-<h1>{$page.url.pathname} and {base + "/"}</h1>
 
 <style lang="scss">
   #menu-container {
