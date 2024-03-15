3a14a345c5e2e8355544c0962012f392a55c2a6e
Jenny Sun <xmissj96@gmail.com>
2024-03-15 10:36:29 +0100

    feat: added cursors and make it so when name is clicked, it navigates to home.

--- src/components/MenuItem.svelte
+++ src/components/MenuItem.svelte
@@ -7,24 +7,25 @@
 <li>
   <!-- svelte-ignore a11y-no-static-element-interactions -->
   <!-- svelte-ignore a11y-click-events-have-key-events -->
-  <a id="menu-link" class:selected={selected} on:click|preventDefault={onClick}>
+  <a id="menu-link" class:selected on:click|preventDefault={onClick}>
     {label}
   </a>
 </li>
 
 <style lang="scss">
   #menu-link {
-    text-underline-offset: 6px; 
+    text-underline-offset: 6px;
     text-decoration-thickness: 1px;
     color: #e6d7ff;
     transition: color 0.3s;
+    cursor: pointer;
 
     &:hover {
       text-decoration: underline;
       text-decoration-color: #e6d7ff;
     }
 
-    &:active{
+    &:active {
       text-decoration: underline;
       color: #d2afff;
       text-decoration-color: #d2afff;
