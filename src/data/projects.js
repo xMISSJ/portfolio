7b92ad261003fb947329de4b58d23114bec1d7d4
Jenny Sun <xmissj96@gmail.com>
2024-03-29 14:13:38 +0100

    feat: put project data in separate folder.

---
+++ src/data/projects.js
@@ -0,0 +1,58 @@
+export let projects = [
+  {
+    name: "G-STAR",
+    description: `This is my first ever website and I had the priviledge to create one
+        for G-Star. The website serves as the digital cornerstone for their
+        pioneering campaign, 'G-Star Certified Tailors.' This initiative is
+        driven by a commitment to sustainability, urging customers to choose
+        repair over replacement for their denim.By really getting behind this
+        idea,G-Star aims to significantly reduce waste and encourage a more
+        sustainable fashion cycle. Located across the Netherlands, G-Star
+        Certified Tailors are skilled artisans ready to breathe new life into
+        your beloved jeans. The website I created not only connects customers
+        with these expert tailors but does so through an engaging,
+        user-friendly platform. Key features include a stylishly integrated
+        Google Maps for effortlessly locating tailors, alongside a dynamic
+        parallax carousel that adds a layer of depth and engagement to the
+        browsing experience. These creative elements ensure the website not
+        only functions smoothly but also captivates and inspires visitors with
+        its design.`,
+    link: "https://www.g-star.com/en_nl/certified-tailors",
+    skills: ["JavaScript", "Svelte", "CSS", "HTML"],
+    images: [
+      {
+        src: "/images/gstar/g-star-certified-tailors-1.jpeg",
+        alt: "G-Star Example 1",
+      },
+    ],
+  },
+  {
+    name: "Desperados Dance Club",
+    description: `An app created for Desperados, and a succesor of the original 
+        'Rave to Save' project. Imagine this: you're at an event, phone in hand, 
+        grooving to the beat. With our app running, every move you make racks up steps. 
+        But here's where it gets interesting. Hit certain step goals and you're in for 
+        a treat — free beer and other event goodies. Plus, you can dance for a cause,
+        turning those steps into donations, all on Desperados' tab. It's not
+        just an app; it's a party with a purpose. We've taken fundraising to a
+        whole new level, making giving back as fun as hitting the dance floor.
+        Get ready to dance, donate, and make a difference, all with a swipe
+        and a shimmy.`,
+    link: "https://wearepi.com/project/desperados-dance-club",
+    skills: ["Flutter", "JavaScript", "Dart"],
+    images: [
+      {
+        src: "/images/desperados/we-are-pi-desperados-1.jpg",
+        alt: "Desperados Wall",
+      },
+      {
+        src: "/images/desperados/we-are-pi-desperados-2.jpeg",
+        alt: "Desperados Party 1",
+      },
+      {
+        src: "/images/desperados/we-are-pi-desperados-3.jpeg",
+        alt: "Desperados Party 2",
+      },
+    ],
+  },
+];
