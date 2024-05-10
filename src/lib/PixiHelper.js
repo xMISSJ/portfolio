b3b26fee7150313f5d3c33672a6df6208fa7eab3
Jenny Sun <xmissj96@gmail.com>
2024-05-10 20:44:57 +0200

    feat: change way of rendering texture as it's deprecated.

--- src/lib/PixiHelper.js
+++ src/lib/PixiHelper.js
@@ -73,9 +73,7 @@ export default class PixiHelper {
           resolution: 1,
         });
 
-        this.app.renderer.render(renderSprite, {
-          renderTexture: renderTexture,
-        });
+        this.app.render(renderSprite, { renderTexture: renderTexture });
 
         sprite = new PIXI.Sprite(renderTexture);
         sprite.x = xPos;
