99fe3035ba7604decba21b4037745c25d77a84f5
xMISSJ <xmissj96@gmail.com>
2026-05-05 16:34:28 +0200

    fix: project header

--- src/components/Projects/ProjectHeader.svelte
+++ src/components/Projects/ProjectHeader.svelte
@@ -1,28 +1,16 @@
 <script lang="ts">
   import SpinningIcon from "../SpinningIcon.svelte";
   import Typography from "../Typography.svelte";
-
-  let headerText: HTMLElement;
-  let newWidth: number = 0;
-
-  $: {
-    if (headerText) {
-      newWidth = headerText.getBoundingClientRect().width + 70;
-    }
-  }
 </script>
 
 <div>
-  <div class="flex flex-1 items-center justify-between">
-    <div class="text-holder" bind:this={headerText}>
+  <div class="flex flex-1 items-center gap-6">
+    <div class="text-holder">
       <Typography variant="h1" type="title2" whiteSpace="nowrap">
         <slot />
       </Typography>
     </div>
     <SpinningIcon />
-    <div
-      class="h-[2px] bg-[#ccc]"
-      style="width: calc(100% - {headerText ? newWidth : 0}px)"
-    ></div>
+    <div class="h-[2px] flex-1 bg-[#ccc]"></div>
   </div>
 </div>
